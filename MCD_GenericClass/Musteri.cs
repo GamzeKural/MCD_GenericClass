3c9c8a68ef41fe659bce6f7daba545a09a9ccbd9
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-07 20:13:41 +0300

    Add project files.

---
+++ MCD_GenericClass/Musteri.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_GenericClass
+{
+    public class Musteri
+    {
+        public int id { get; set; }
+        public string musteriNumara { get; set; }
+        public string TcKimlikNumarasi { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public DateTime dogumTarihi { get; set; }
+
+    }
+}
---
+++ MCD_GenericClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.id = 1;
+            m1.TcKimlikNumarasi = "12345678901";
+            m1.musteriNumara = "MS123";
+            m1.isim = "Gamze";
+            m1.soyisim = "Kural";
+            m1.dogumTarihi = DateTime.Parse("07.01.1998");
+
+            MusteriGeneric<int> musteriGeneric1 = new MusteriGeneric<int>();
+            musteriGeneric1.id = 1;
+            musteriGeneric1.musteriNumarasiAl();
+
+            MusteriGeneric<Guid> musteriGeneric2 = new MusteriGeneric<Guid>();
+            musteriGeneric2.musteriNumarasiAl();
+
+            Console.ReadKey();
+        }
+    }
+}
